952d0fe60fbfbce0920d175f6690e4b49304bf92
3looloo <aliabdelfatah455@gmail.com>
2024-10-01 07:19:02 +0300

    mod

--- Movie/src/components/Casts/MovieCasts.jsx
+++ Movie/src/components/Casts/MovieCasts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchMovieCasts } from "../../services/GetMovies";
+import { fetchMovieCasts } from "../../services/getMovies";
 
 export const MovieCasts = () => {
   const { id } = useParams();
